2b90b0d537634d78ef354ca21f7e2e22e856be57
Roberta Doyle <ro.scalabrin@gmail.com>
2016-05-19 17:54:33 -0600

    Added more tests to check the bubble sort algorithm

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,24 +1,21 @@
 class BubbleSort
 attr_accessor :previous, :current, :unsorted_array
 
-  def initialize(unsorted_array)
+  def initialize(unsorted_array = [])
     @unsorted_array = unsorted_array
-    @previous
-    @current
   end
 
   def sort(counter = 0)
   @previous = unsorted_array[counter]
   @current = unsorted_array[counter + 1]
-  count = unsorted_array.count - 1
-    if previous == unsorted_array[count]
-      p @unsorted_array
+    if previous == unsorted_array[unsorted_array.count - 1]
+      unsorted_array
     else
       if previous > current
         unsorted_array.delete(current)
         @unsorted_array = unsorted_array.insert(counter, current)
       end
-      counter = counter += 1
+      counter += 1
       sort(counter)
     end
   end
--- test/bubble_sort_test.rb
+++ test/bubble_sort_test.rb
@@ -11,25 +11,41 @@ class BubbleSortTest < Minitest::Test
     assert_instance_of BubbleSort, sorter
   end
 
+  def test_sorter_if_input_is_nil
+    sorter = BubbleSort.new
+    assert_instance_of BubbleSort, sorter
+  end
 
-  def test_sorting_with_different_array_sizes
+  def test_sorting_numbers_in_the_array
     sorter = BubbleSort.new([ 2, 0, 1, 3, 4, 5 ])
-      binding.pry
     sorter.sort
     assert_equal [ 0, 1, 2, 3, 4, 5 ], sorter.sort
   end
 
-  def test_sorting_with_different_empty_array
-  skip
+  def test_sorting_letters_in_the_array
+    sorter = BubbleSort.new([ "d", "b", "a", "c"])
+    sorter.sort
+    assert_equal [ "a", "b", "c", "d" ], sorter.sort
   end
 
-  def test_sorting_with_different_letters
-  skip
+  def test_sorting_lowercase_and_upcase_letters_in_the_array #not working
+    sorter = BubbleSort.new([ "A", "c", "f", "B", "d", "E" ])
+    sorter.sort
+    assert_equal [ "A", "B", "c", "D", "E", "f" ], sorter.sort
   end
 
-  def test_sorting_with_numbers_and_letters
-  skip
+  def test_sorting_numbers_and_letters_in_the_array #not working
+    skip
+    sorter = BubbleSort.new([ "A", "C", "3", "B", "2", "1" ])
+    sorter.sort
+    assert_equal [ "A", "B", "c", "1", "2", "3" ], sorter.sort
   end
 
+  def test_sorting_two_letters_in_the_same_element
+    skip
+    sorter = BubbleSort.new([ "A", "C", "3", "B", "2", "1" ])
+    sorter.sort
+    # assert_equal [ "A", "B", "c", "1", "2", "3" ], sorter.sort
+  end
 
 end
